32af0d9f812d3cde8b8ecf52d57b7b7725e56030
dinudisachithma <dinudisachithma@gmail.com>
2026-04-26 20:28:52 +0530

    Delete src/main/java/com/gymmanagementsystem/aiproject/repository/TrainerRepository.java
    remove wrongly pushed TrainerReporsitory.java file

--- src/main/java/com/gymmanagementsystem/aiproject/repository/TrainerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gymmanagementsystem.aiproject.repository;
-
-import com.gymmanagementsystem.aiproject.model.Trainer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TrainerRepository extends JpaRepository<Trainer, String> {
-}
